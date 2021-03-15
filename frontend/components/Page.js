1ec4d75abf661794c4f5e904ed08098ed51d9ded
alina-kontarero <alina.kontarero@gmail.com>
2021-03-15 11:24:01 +1100

    Finished section #2 (Next.js)

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,22 @@
-export default function Page() {
-  return <div>
-    <h2>I'm the Page Component! </h2>
-  </div>
+import PropTypes from 'prop-types';
+import Header from './Header'
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <Header />
+      <h2>I'm the Page Component! </h2>
+      <code>{cool}</code>
+      {children}
+    </div>
+  );
 }
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  // children: PropTypes.oneOf([
+  //   PropTypes.arrayOf(PropTypes.node),
+  //   PropTypes.node,
+  // ]),
+  children: PropTypes.any,
+};
